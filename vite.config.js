3937a4fa1837cbb9ae42766ad1be5cb1231f6179
AshokSidhid <152054898+AshokSidhid@users.noreply.github.com>
2025-09-15 10:56:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/jenkinsappdemo/',
     server: {
     port: 5888 // Change this to your desired port
   }
